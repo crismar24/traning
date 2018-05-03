09c619870af34ec18dec89da54485f1ef397293a
crismar24 <crismar24@gmail.com>
2018-05-03 10:16:20 +0300

    init project .

--- src/collection/main.java
+++ src/collection/main.java
@@ -1,38 +1,37 @@
 package collection;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.*;
 import java.util.List;
-import java.util.TreeSet;
 
 public class main {
     public static void main(String[] args) {
 
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> arrayList = new ArrayList<String>();
         for (int i = 0; i < 10; i++) {
-            list.add("name" + i);
+            arrayList.add("name" + i);
         }
+        System.out.println("arrayList \n\r" + arrayList);
+
+        SortedSet sortedSet = new TreeSet<Integer>();
+        for (int i = 0; i < 10; i++) {
+            sortedSet.add("name" + i);
+        }
+        System.out.println("sortedSet \n\r" + sortedSet);
 
         TreeSet<String> treeSet = new TreeSet<String>();
         for (int i = 0; i < 10; i++) {
             treeSet.add("name" + i);
         }
-
-        System.out.println( list.containsAll(treeSet) );
+        System.out.println("treeSet \n\r" + treeSet );
 
         //Хорош для поиска дубликатов
         HashSet hashSet = new HashSet();
         for (int i = 0; i < 10; i++) {
             hashSet.add("name" + i);
         }
+        System.out.println("hashSet \n\r" + hashSet);
 
-        for (Object hash : hashSet) {
-
-            hash.hashCode()
-            hash.equals()
-
-        }
+        List<Integer> integerList = new ArrayList<Integer>();
 
 
 
